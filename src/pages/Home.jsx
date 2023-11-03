7e16348634c88d1b02109a060f1bd5ed28318500
Jay9874 <jayprakashsharma225@gmail.com>
2023-11-04 01:24:10 +0530

    home color update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -144,7 +144,7 @@ export default function Home () {
               <div className='mt-10 flex-wrap flex items-center justify-center gap-x-6'>
                 <Link
                   to='/auth'
-                  className='rounded-md bg-green-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-400'
+                  className='rounded-md bg-yellow-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-400'
                 >
                   Get started
                 </Link>
