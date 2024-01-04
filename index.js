4b205a9917f37f38c1108584b2f111422f06ce15
Jay9874 <jayprakashsharma225@gmail.com>
2024-01-04 23:51:45 +0530

    Add frontend route to server

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ app.use(express.json({ limit: '25mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(cors())
-app.use(express.static(path.join(__dirname, '../client', 'build')))
+app.use(express.static(path.join(__dirname, 'frontend', 'build')))
 
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
   console.log('got request on root route')
   res.send('Hello world')
 })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'frontend', 'build', 'index.html'),
+      function (err) {
+        if (err) {
+          res.status(500).send(err)
+        }
+      }
+    )
+  })
+}
 
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`)
