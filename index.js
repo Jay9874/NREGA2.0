41c487184bf51609ba9b59edd6a099b51fc6b069
Jay9874 <jayprakashsharma225@gmail.com>
2024-01-04 23:42:09 +0530

    Add backend deployemnt settings

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
-const app = express()
 const port = process.env.PORT || 8080
+const cors = require('cors')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(express.json({ limit: '25mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../client', 'build')))
 
 app.get('/', (req, res) => {
   console.log('got request on root route')
