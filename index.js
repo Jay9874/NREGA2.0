45442410e1166d683635ca716f4d58779a3172fe
Jay9874 <jayprakashsharma225@gmail.com>
2024-01-06 23:18:14 +0530

    Add frontend path

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')))
-
 app.get('/api/hello', (req, res) => {
   console.log('got request on root route')
   res.send('Hello world')
@@ -22,14 +21,11 @@ app.get('/api/recovery', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, 'frontend', 'dist', 'index.html'),
-    function (err) {
-      if (err) {
-        res.status(500).send(err)
-      }
+  res.sendFile('index.html', function (err) {
+    if (err) {
+      res.status(500).send(err)
     }
-  )
+  })
 })
 
 app.listen(port, () => {
