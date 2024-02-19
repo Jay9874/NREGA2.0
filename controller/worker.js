35dece6448319e4b363648ee2e375d3b7beeb324
Jay9874 <jayprakashsharma225@gmail.com>
2024-02-19 23:03:24 +0530

    backend: controller update

--- controller/worker.js
+++
@@ -1,4 +0,0 @@
-const createClient = require('@supabase/supabase-js')
-const supabaseUrl = process.env.VITE_SUPABASE_URL
-const supabaseKey = process.env.VITE_SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
