f3eabcf7f3ab3ce3ac902871e18cea77fbafea83
Jay9874 <jayprakashsharma225@gmail.com>
2024-12-06 18:44:17 +0530

    admin: attendance table upgrade

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -10,7 +10,7 @@ export default function Banner({ text, link }) {
   return (
     <div
       ref={closeRef}
-      className='relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1'
+      className='relative z-30 isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1'
     >
       <div
         className='absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
